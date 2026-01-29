b8f704e2fee952f83dd91c21b342b94559804974
RanchanadeviS <ranchanadevi20@gmail.com>
2026-01-29 16:10:13 +0530

    Updated backend URL for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = "https://your-backend-deployment-url.com";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://cattle-connect.onrender.com";
 

7eec6551e2700f3e7f200a097299c50c4a9692ec
RanchanadeviS <ranchanadevi20@gmail.com>
2026-01-29 14:26:06 +0530

    Fixed base path for Netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,19 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // ✅ Fix: Use normal base path for localhost
-  base: "/",
-
+  base: "./",   // ✅ Correct for Netlify
   build: {
     outDir: "docs",
   },
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
   plugins: [react()],
 });
-
